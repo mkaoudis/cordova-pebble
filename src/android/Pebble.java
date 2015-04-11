b23fb060abcabb91cad14e7087ca6e9c59a6ebff
mkaoudis <michael.kaoudis@colorado.edu>
2015-04-11 11:25:08 -0600

    added isWatchConnected()

--- src/android/Pebble.java
+++ src/android/Pebble.java
@@ -7,10 +7,21 @@ import org.json.JSONException;
 import com.getpebble.android.kit.*;
 import com.getpebble.android.kit.util.*;
 
+import java.util.UUID;
+
 public class Pebble extends CordovaPlugin {
 
+	@Override
+	public boolean execute(String action, JSONArray args, final CallbackContext cb) throws JSONException {
 
+		Context context=this.cordova.getActivity().getApplicationContext();
 
+		if (action.equals("isWatchConnected")){
+            cb.success(PebbleKit.isWatchConnected(context) ? 1 : 0);
+            return true;
+        }
 
-
+        //if no action found
+        return false;
+	}
 }
---
+++ www/pebble.js
@@ -0,0 +1,12 @@
+var exec = require('cordova/exec');
+
+var Pebble = function() {
+
+};
+
+var Pebble.isWatchConnected = function (success, error){
+	if (!success) return console.error('success callback is required');
+	cordova.exec(success, error || genericError, 'Pebble', 'isWatchConnected', []);
+};
+
+module.exports = Pebble;
